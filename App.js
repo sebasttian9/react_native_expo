9b9a28d9b2f85d4fabb1f93e169ea3b38d8a3db3
Sebastian Valenzuela <svalenzuela.ortiz@gmail.com>
2023-11-14 13:47:17 -0300

    Fetch productos test

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, Pressable, ActivityIndicator, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getProducts = async () => {
+    try {
+      const response = await fetch('https://www.autotec.cl/json/productos.php');
+      const json = await response.json();
+      setData(json.productos);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+  
   return (
+    <>
     <View style={styles.container}>
-      <Text>Prueba seba2</Text>
+      <Pressable 
+        onPress={() => alert('presionado')} 
+        // Alert.alert('Hemos tocado el texto')
+      >
+        <Text>Prueba seba2</Text>
+      </Pressable>
       <Text>Open up App.js to start working seba on your app!</Text>
-      <StatusBar style="auto" />
     </View>
+    <View style={{flex: 1, padding: 24}}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({prod_id}) => prod_id}
+          renderItem={({item}) => (
+            <Text>
+              {item.prod_id}, {item.prod_nombre}
+            </Text>
+          )}
+        />
+      )}
+    </View>
+    </>
   );
 }
 
